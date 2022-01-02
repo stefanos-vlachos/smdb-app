5d6afd4c7aba908e84d95712e6925439312fc42a
kmoulosiotis <kostas.moulosiotis@gmail.com>
2022-01-02 12:52:39 +0200

    Fixed genre and role enums

--- src/main/java/com/pfseven/smdb/domain/Genre.java
+++ src/main/java/com/pfseven/smdb/domain/Genre.java
@@ -1,30 +1,10 @@
 package com.pfseven.smdb.domain;
 
 public enum Genre {
-    DRAMA, COMEDY, ANIMATION, THRILLER, ADVENTURE, ROMANCE, WESTERN, CRIME, BIOGRAPHY, ACTION, HISTORY;
+    DRAMA, COMEDY, ANIMATION, THRILLER, ADVENTURE, ROMANCE, WESTERN, CRIME, BIOGRAPHY, ACTION, HISTORY, FANTASY, FAMILY, SCI_FI;
 
     public static Genre stringCompare(String str){
-        if(str.equalsIgnoreCase(DRAMA.toString().toLowerCase()))
-            return DRAMA;
-        else if(str.equalsIgnoreCase(COMEDY.toString().toLowerCase()))
-            return COMEDY;
-        else if(str.equalsIgnoreCase(ANIMATION.toString().toLowerCase()))
-            return ANIMATION;
-        else if(str.equalsIgnoreCase(THRILLER.toString().toLowerCase()))
-            return THRILLER;
-        else if(str.equalsIgnoreCase(ADVENTURE.toString().toLowerCase()))
-            return ADVENTURE;
-        else if(str.equalsIgnoreCase(ROMANCE.toString().toLowerCase()))
-            return ROMANCE;
-        else if(str.equalsIgnoreCase(WESTERN.toString().toLowerCase()))
-            return WESTERN;
-        else if(str.equalsIgnoreCase(CRIME.toString().toLowerCase()))
-            return CRIME;
-        else if(str.equalsIgnoreCase(BIOGRAPHY.toString().toLowerCase()))
-            return BIOGRAPHY;
-        else if(str.equalsIgnoreCase(ACTION.toString().toLowerCase()))
-            return ACTION;
-        return HISTORY;
+        return Genre.valueOf((str).toUpperCase().replace('-','_'));
     }
 
 }
--- src/main/java/com/pfseven/smdb/domain/Role.java
+++ src/main/java/com/pfseven/smdb/domain/Role.java
@@ -4,14 +4,6 @@ public enum Role {
     ACTOR, DIRECTOR, WRITER, PRODUCER, MUSICIAN;
 
     public static Role roleCompare(String str){
-        if (str.equalsIgnoreCase(ACTOR.toString()))
-            return ACTOR;
-        else if (str.equalsIgnoreCase(DIRECTOR.toString()))
-            return DIRECTOR;
-        else if (str.equalsIgnoreCase(WRITER.toString()))
-            return WRITER;
-        else if (str.equalsIgnoreCase(PRODUCER.toString()))
-            return PRODUCER;
-        return MUSICIAN;
+        return Role.valueOf((str).toUpperCase().replace('-','_'));
     }
 }
